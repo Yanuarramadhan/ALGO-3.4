d30b978ca30439757501ebcaafb006b704b6f1f3
Yanuar <45526227+Yanuarramadhan@users.noreply.github.com>
2018-12-13 16:14:44 +0700

    Add files via upload

---
+++ soal4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float C,R,F;
+    cout<<"program konversi suhu"<<endl;
+    cout<<"yayan yanuar r"<<endl<<endl;
+    cout<<"Masukan nilai celciusnya=";
+    cin>>C;
+    F=9/5*C+32;
+    R=4/5*C;
+    cout<<"nilai Fahrenheit="<<F<<endl<<"nilai Reamur="<<R<<endl;
+    cin.get();
+}
